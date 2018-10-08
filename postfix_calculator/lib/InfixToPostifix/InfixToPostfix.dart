db961b1a63b8dc68e6a2ec0b505bf0bd5edc1090
Mattia Rubini <mattia.rubini@gmail.com>
2018-10-08 08:29:55 +0200

    Added the infix to postfix function, also, the test class was setup

---
+++ postfix_calculator/lib/InfixToPostifix/InfixToPostfix.dart
@@ -0,0 +1,99 @@
+// Unicode UTF-16 table: https://www.fileformat.info/info/charset/UTF-16/list.htm
+
+/// Take a string containing an inifix equation and convert it to a postfix equation
+/// All number in the infix equation must be integers
+String infixToPostfix(String infixEquation){
+    // Strings are mede of runes (Unicode UTF-16 code units (int containing the decimal value of UTF-16))
+    // String are immutable, use a string buffer to crate a string
+    StringBuffer strBuff = new StringBuffer();
+    // Stack containing the operators
+    List operatorStack = [];
+    // Check every rune of the infix equation
+    for (int i=0; i<infixEquation.length; i++){
+      // push = add to the stack
+      // pop = remove the last added element of the stack
+      // If a digit is found, keep printing digit until there are no more digit to print
+      if (isNumber(infixEquation.codeUnitAt(i))) {
+        do{
+          strBuff.write(infixEquation[i]);
+          i++;
+        // If the string ends with a number (and it's very likely) rember to not access the string beyond it's lenght
+        }while((i<infixEquation.length) && (isNumber(infixEquation.codeUnitAt(i))));
+        strBuff.write(' ');
+        // Must rewind i to the element of the string
+        if (i >= infixEquation.length) i = infixEquation.length-1;
+      } // Once the digit are all printed, I must check what is the next character, and what to do with it
+      // If an open parenthesis is found, push it to the stack
+      if (infixEquation[i] == '('){
+        operatorStack.add(infixEquation[i]);
+      }
+      // If a closed parenthesis is found, pop all operators, from the stack, until an open parenthesis is found
+      if(infixEquation[i] == ')'){
+        do{
+          strBuff.write(operatorStack.removeLast()+' ');
+        }while(operatorStack.last != '(');
+        // Discarding the opern parenthesis
+        operatorStack.removeLast();
+      }
+      // If an operator is found
+      if ( isOperator(infixEquation[i]) ){
+        // If the stack is empy, or the precedence of the actual operator is greater than the last operator in the stack
+        // push the operator in the stack
+        if ((operatorStack.isEmpty) || (operatorPriority(infixEquation[i]) > operatorPriority(operatorStack.last))){
+          operatorStack.add(infixEquation[i]);
+        } else { // Empty the stack, until it's empty or the last operator (of the stack) has less precedence of the current operator
+          do{
+            strBuff.write(operatorStack.removeLast() + ' ');
+          } while((operatorStack.isNotEmpty) && (operatorPriority(infixEquation[i]) < operatorPriority(operatorStack.last)));
+          // When done push the operator at hand
+          operatorStack.add(infixEquation[i]);
+        }
+      }
+    }
+    // Pop the remaining content of the stack
+    while(operatorStack.isNotEmpty){
+      strBuff.write(operatorStack.removeLast() + ' ');
+    }
+    return strBuff.toString();
+}
+
+
+
+/// Take a rune (integer rappresenting Unicode code point)
+/// Return true if the rune is a number or a dot
+bool isNumber (int i){
+  // Numbers are between 48 (0) and 57 (9)
+  if ( (i >= 48 && i <= 57) || (i == 46) ) return true;
+  return false;
+}
+
+
+
+/// Return the operator priority
+int operatorPriority(String c){
+  switch(c){
+
+    // case ^
+    case '^': return 3;
+
+    // case * /
+    case '*': return 2;
+    case '/': return 2;
+
+    // case + -
+    case '+': return 1;
+    case '-': return 1;
+
+    // default
+    default: return -1;
+
+  }// switch
+} // operatorPriority
+
+
+
+/// True if the char is an operator
+bool isOperator(String op){
+  if ( operatorPriority(op) > -1 ) return true;
+  return false;
+}
---
+++ postfix_calculator/test/unit_test.dart
@@ -0,0 +1,39 @@
+// With unit test I am testing the logical part of the app (conversion from infix to postfix)
+
+import 'package:test/test.dart';
+
+import 'package:postfix_calculator/InfixToPostifix/InfixToPostfix.dart';
+
+//import 'lib/InfixToPostfix/InfixToPostfix.dart';
+
+void main() {
+
+  test(
+    "Testing the operator priority",
+    (){
+      // Non mapped char should return -1
+      expect(operatorPriority('?'), -1);
+      // Checking operatorPriority
+      assert(operatorPriority('*') > operatorPriority('+'));
+      expect(operatorPriority('+'), operatorPriority('-'));
+      //Checking isNumber
+      // The first 11 elements must return true
+      String str = "0123456789.pgftxzbaqw";
+      for (int i=0; i<11; i++){
+        assert(isNumber(str.codeUnitAt(i)));
+      }
+      // From the 12th element to the end of the string, isNumber must return false
+      for (int i=11; i<str.length; i++){
+        assert(!isNumber(str.codeUnitAt(i)));
+      }
+      // Checking infixToPostfix
+      expect(infixToPostfix("1*2+3"), "1 2 * 3 + ");
+      expect(infixToPostfix("1+2*3"), "1 2 3 * + ");
+      expect(infixToPostfix("1*(2+3)"), "1 2 3 + * ");
+      expect(infixToPostfix("1-2+3"), "1 2 - 3 + ");
+      expect(infixToPostfix("1*2^3+4"), "1 2 3 ^ * 4 + ");
+      expect(infixToPostfix("1*(2+3*4)+5"), "1 2 3 4 * + * 5 + ");
+    }
+  );
+
+}// Main
